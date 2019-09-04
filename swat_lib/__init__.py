d6b45275dc1f4fc977e47a5c8589566ce25d3bb7
ben-farrelly <benjamin.farrelly@serato.com>
2019-09-04 15:21:05 +1200

    Tweaking pathing

--- swat_lib/__init__.py
+++ swat_lib/__init__.py
@@ -1,5 +1,7 @@
 # Import all files to be accessed
-from .utilities import *
+# from .utilities.framework import *
+from swat_lib.utilities.framework import *
+from swat_lib.utilities.emails import *
 
 
 name = 'swat_lib'
